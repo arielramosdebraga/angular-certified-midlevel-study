d31de15373220964fe4ad376d8e16a107a072818
Ariel Ramos de Braga <bragaariel@gmail.com>
2025-11-23 19:58:37 -0300

    Adicionado novas configurações para o jest

--- src/jest.config.js
+++ src/jest.config.js
@@ -8,4 +8,21 @@ module.exports = {
   moduleFileExtensions: ['ts', 'html', 'js', 'json'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+    collectCoverageFrom: [
+    'src/**/*.{ts,js}',
+    '!src/**/*.spec.{ts,js}', // Exclui arquivos de teste da contagem de cobertura
+    '!src/main.ts', // Exclui arquivos que não são tipicamente testados
+    '!src/polyfills.ts',
+    '!src/test.ts',
+    '!src/environments/**',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80, // Exige 80% de cobertura de statements (declarações)
+      branches: 75,   // Exige 75% de cobertura de branches (ramificações, ex: if/else)
+      functions: 80,  // Exige 80% de cobertura de functions
+      lines: 80,      // Exige 80% de cobertura de lines
+    },
+  },
 };
