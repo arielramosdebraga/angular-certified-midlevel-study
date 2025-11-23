aacc885b9324b4c83703b8ada4a97108ab25593b
Ariel Ramos de Braga <bragaariel@gmail.com>
2025-11-23 00:02:43 -0300

    component layout das páginas internas

---
+++ src/app/pages/template/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'layout',
+  imports: [RouterOutlet],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
---
+++ src/app/pages/template/template.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+
+export const ROUTES: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'categorias',
+        loadChildren: () =>
+          import('../categorias/categoria.routes').then(
+            m => m.ROUTES
+          ),
+      }
+    ]
+  },
+];
